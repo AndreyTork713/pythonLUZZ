1c9283de316d7938decb60b7996a7363fd6f10e1
Andrey Kropotin <tork713@gmail.com>
2020-09-17 17:48:25 +0700

    вариант 1

--- lock.py
+++ lock.py
@@ -1,1 +1,20 @@
 # ЗАмок с управлением по числовому паролю
+
+import time
+
+lock = 0
+electro = 1
+while electro == 1:
+    print('Введите пароль: ')
+    pass1 = int(input())
+
+    password = (pass1)
+    if password == (2865):
+        lock = 1
+        print('дверь открыта')
+        time.sleep(5)
+        password = ()
+        lock = 0
+    else:
+        print('не верный пароль, дверь закрыта')
+
