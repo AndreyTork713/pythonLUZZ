acad4cdd334c8432981360f16b4241b63526cf80
Andrey Kropotin <tork713@gmail.com>
2020-09-18 07:41:08 +0700

    18.09.2020 7:40 Корректировка применения фукции автомата

--- lock.py
+++ lock.py
@@ -5,21 +5,19 @@ import dop
 
 el = input('Enter ON or OFF')
 
-dop.el_automat(el)
-
 lock = 0
-electro = 1
+electro = dop.el_automat(el)
 while electro == 1:
     print('Введите пароль: ')
     pass1 = int(input())
-    password = (pass1)
-    if password == (2865):
+    password = pass1
+    if password == 2865:
         lock = 1
         print('дверь открыта')
         time.sleep(5)
         password = ()
         lock = 0
-    elif password == (3105704888):
+    elif password == 3105704888:
         lock = 1
         print('Программа остановлена, дверь разблокирована')
         time.sleep(5)
